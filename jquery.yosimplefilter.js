3167b8e37f9ff41be499b381789908fc815d1727
Chris Johnson <chrisltd@gmail.com>
2013-03-15 21:48:58 -0400

    Updated version line

--- jquery.yosimplefilter.js
+++ jquery.yosimplefilter.js
@@ -1,7 +1,7 @@
 // By Chris Johnson
 // http://chrisltd.com
 // Created March 2013
-// Version .01b
+// Version .01
 // Yo Simple Filter does simple single category filtering on a set of elements. Just activate the plugin on a properly formatted wrapper element, make sure you have a proper set of filters and you're good to go. Alternatively, you can pass settings to match your custom HTML structure.
 
 (function( $ ){
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # Yo Simple Filter
-## Version .01b
+### Version .01 | By [Chris Johnson](http://chrisltd.com) | https://github.com/ChrisLTD/yo_simple_filter
+
 Yo Simple Filter does simple single category filtering on a set of elements. 
 
 ![Animated Example](https://github.com/chrisltd/yo_simple_filter/raw/master/example.gif)
